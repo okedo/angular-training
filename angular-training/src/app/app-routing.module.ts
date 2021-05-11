448c65e250efe314b078c13af46a10ccb2c590ae
Viktor Patraboi <viktor.patraboi@medhost.com>
2021-05-11 14:06:55 +0300

    add routing

--- angular-training/src/app/app-routing.module.ts
+++ angular-training/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './features/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./features/dashboard/dashboard.module').then(module => module.DashboardModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-training/src/app/features/home/home-routing.module.ts
+++ angular-training/src/app/features/home/home-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [{
-  path: '',
-  component: DashboardComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class HomeRoutingModule { }
